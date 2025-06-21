8d1aafa2bee6441a4cad47e799148ad3fe226194
Eterill <eterill@hotmail.com>
2025-06-21 10:47:06 +1000

    Delete functions to fit 1.20.1

--- src/client/java/lpctools/tools/ToolConfigs.java
+++ src/client/java/lpctools/tools/ToolConfigs.java
@@ -4,8 +4,6 @@ import lpctools.lpcfymasaapi.configbutton.derivedConfigs.ThirdListConfig;
 import lpctools.lpcfymasaapi.implementations.ILPCConfigList;
 import lpctools.tools.autoGrindstone.AutoGrindstone;
 import lpctools.tools.antiSpawner.AntiSpawner;
-import lpctools.tools.canSpawnDisplay.CanSpawnDisplay;
-import lpctools.tools.slightXRay.SlightXRay;
 import lpctools.tools.fillingAssistant.FillingAssistant;
 import lpctools.tools.liquidCleaner.LiquidCleaner;
 
@@ -14,10 +12,8 @@ import static lpctools.lpcfymasaapi.LPCConfigStatics.*;
 public class ToolConfigs {
     static ThirdListConfig FAConfig;
     static ThirdListConfig LCConfig;
-    static ThirdListConfig SXConfig;
     static ThirdListConfig AGConfig;
     static ThirdListConfig ASConfig;
-    static ThirdListConfig CSConfig;
     public static void init(){
         ILPCConfigList lastList = peekConfigList();
         try(ConfigListLayer layer = new ConfigListLayer()){
@@ -25,14 +21,10 @@ public class ToolConfigs {
             FillingAssistant.init();
             layer.set(LCConfig = addThirdListConfig(lastList, "LC", false));
             LiquidCleaner.init();
-            layer.set(SXConfig = addThirdListConfig(lastList, "SX", false));
-            SlightXRay.init();
             layer.set(AGConfig = addThirdListConfig(lastList, "AG", false));
             AutoGrindstone.init();
             layer.set(ASConfig = addThirdListConfig(lastList, "AS", false));
             AntiSpawner.init();
-            layer.set(CSConfig = addThirdListConfig(lastList, "CS", false));
-            CanSpawnDisplay.init();
         }
     }
 }
